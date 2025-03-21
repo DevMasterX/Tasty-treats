f8c1c168f4d872ed150c80d8a32ced02754c9959
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-03-21 23:20:16 +0000

    Deploying to gh-pages from @ DevMasterX/Tasty-treats@d27b7e0deccf02201fff2c73f28afb07e84aaf0b 🚀

---
+++ index.23fe0afd.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-theme-switcher__input");console.log("\uD83D\uDE80 themeSwitcherInput:",e),e.addEventListener("change",function(){document.body.classList.toggle("dark-theme")});
+//# sourceMappingURL=index.23fe0afd.js.map
---
+++ index.26ca8744.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelector(".js-theme-switcher__input");console.log("\uD83D\uDE80 themeSwitcherInput:",e),e.addEventListener("change",function(){document.body.classList.toggle("dark-theme")})}();
+//# sourceMappingURL=index.26ca8744.js.map
