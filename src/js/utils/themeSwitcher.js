018c2d5fd8ffa13d1683d7455d10f8e3e0472b84
DevMasterX <sokol080288@gmail.com>
2025-03-22 02:31:28 +0200

    theme js with saving to local storage

--- src/js/utils/themeSwitcher.js
+++ src/js/utils/themeSwitcher.js
@@ -1,8 +1,31 @@
 const themeSwitcherInput = document.querySelector('.js-theme-switcher__input');
-console.log('🚀 themeSwitcherInput:', themeSwitcherInput);
+const DARK = 'dark-theme';
+const THEME = 'theme';
+
+initTheme();
 
 themeSwitcherInput.addEventListener('change', onSwitherInputChange);
 
 function onSwitherInputChange() {
-  document.body.classList.toggle('dark-theme');
+  document.body.classList.toggle(DARK);
+
+  if (document.body.classList.contains(DARK)) {
+    localStorage.setItem(THEME, DARK);
+  } else {
+    localStorage.removeItem(THEME);
+  }
+}
+
+function initTheme() {
+  try {
+    const savedTheme = localStorage.getItem(THEME);
+    console.log('🚀 savedTheme:', savedTheme);
+    if (!savedTheme) {
+      return;
+    }
+    document.body.classList.add(savedTheme);
+    themeSwitcherInput.checked = true;
+  } catch (error) {
+    console.error('ERROR! Can not find saved theme in local storage', error);
+  }
 }
