778eec7a157c6cbb8922adf9ab0dfe4662872b48
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-03-24 15:51:18 +0200

    mobileMenu.js in progress

--- src/js/helpers/mobileMenu.js
+++ src/js/helpers/mobileMenu.js
@@ -5,12 +5,27 @@ const mobileMenuCloseBtn = document.querySelector('.js-close-btn');
 
 mobileMenuBtn.addEventListener('click', onMobileMenuBtnClick);
 mobileMenuCloseBtn.addEventListener('click', onMobileMenuCloseBtnClick);
+
 function onMobileMenuBtnClick() {
-  mobileMenu.classList.add('open');
-  header.classList.add('menu-opened');
+  onOpenMenu();
 }
 
 function onMobileMenuCloseBtnClick() {
+  onCloseMenu();
+}
+
+function onOpenMenu() {
+  mobileMenu.classList.add('open');
+  header.classList.add('menu-opened');
+  //   document.addEventListener('click', onDocumentClick);
+}
+function onCloseMenu() {
   mobileMenu.classList.remove('open');
   header.classList.remove('menu-opened');
+  //   document.removeEventListener('click', onDocumentClick);
+}
+
+function onDocumentClick(e) {
+  const clickInsideMenu = mobileMenu.contains(e.target);
+  console.log('🚀 clickInsideMenu:', clickInsideMenu);
 }
