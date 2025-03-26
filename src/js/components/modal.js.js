cdb4fda870deb0ca235f07556b48fb630ce3c1ef
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-03-26 16:28:16 +0200

    modal.js in progres

--- src/js/components/modal.js.js
+++ src/js/components/modal.js.js
@@ -2,7 +2,15 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.modal-overlay');
 const modalContent = document.querySelector('.modal-content');
 
-function openModal() {}
+function openModal() {
+  modal.classList.toggle('is-hidden');
+}
 function closeModal() {}
 
 function setModalContent(htmlString) {}
+
+function initModal() {
+  document.querySelectorAll('[data-modal-open]').forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+}
