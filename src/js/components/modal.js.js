cf793441f3f31572150b8b15e212672b527a6f8c
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-03-26 12:27:41 +0200

    modal.js in progres

--- src/js/components/modal.js.js
+++ src/js/components/modal.js.js
@@ -1,3 +1,7 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.modal-overlay');
+const modalContent = document.querySelector('.modal-content');
+
 function openModal() {}
 function closeModal() {}
 
