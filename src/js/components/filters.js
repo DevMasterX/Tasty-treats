717fabfd89981f291e8f496dca31f4fb52bbe1a6
DevMasterX <sokol080288@gmail.com>
2025-04-26 03:12:01 +0300

    76556

--- src/js/components/filters.js
+++ src/js/components/filters.js
@@ -3,6 +3,7 @@ import Choices from 'choices.js';
 const choicesOptions = {
   shouldSort: false,
   searchEnabled: false,
+  itemSelectText: '',
 };
 function initFilters() {
   const selectElements = document.querySelectorAll('.js-choice');
