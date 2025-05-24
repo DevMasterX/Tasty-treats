134a0fa56b3fbd618bd1a78f0f765eac0530f8c0
DevMasterX <sokol080288@gmail.com>
2025-05-25 01:25:30 +0300

    768687

--- src/js/components/galleryMarkup.js
+++ src/js/components/galleryMarkup.js
@@ -10,8 +10,10 @@ function createGalleryMarkup(arr) {
   }
 
   return arr
-    .map(
-      ({ preview, title, description, rating, _id }) => `
+    .map(({ preview, title, description, rating, _id }) => {
+      const paintedStarsWidth = (rating / 5) * 100;
+
+      return `
      <li class="gallery-item">
 <a href="${preview}" class="gallery-lightbox"  data-width="700px"
   data-height="auto" data-zoomable="true" data-type="image"
@@ -29,13 +31,65 @@ function createGalleryMarkup(arr) {
       <div class="rating-and-btn-wrapper">
         <div class="rating-wrapper">
           <p class="rating-number">${rating}</p>
+          <div class="stars-list-wrapper">
           <ul class="rating-list">
-            <li class="rating-list__item"></li>
-            <li class="rating-list__item"></li>
-            <li class="rating-list__item"></li>
-            <li class="rating-list__item"></li>
-            <li class="rating-list__item"></li>
+            <li class="rating-list__item">
+             <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+            <li class="rating-list__item">
+            <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+            <li class="rating-list__item">
+            <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+            <li class="rating-list__item">
+            <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+            <li class="rating-list__item">
+            <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
           </ul>
+
+ <ul class="rating-list painted" style="width: ${paintedStarsWidth}%;">
+            <li class="rating-list__item">
+             <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+            <li class="rating-list__item">
+            <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+            <li class="rating-list__item">
+            <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+            <li class="rating-list__item">
+            <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+            <li class="rating-list__item">
+            <svg class="rating-list__star-icon">
+                <use href="#icon-star"></use>
+              </svg>
+            </li>
+          </ul>
+           </div>
+
+
         </div>
         <button class="gallery-item__see-recipe-btn" aria-label="See recipe button" data-modal-open  data-modal-type="recipe"   data-id="${_id}" >See recipe</button>
       </div>
@@ -63,8 +117,8 @@ function createGalleryMarkup(arr) {
 
 
     
-    `
-    )
+    `;
+    })
     .join('');
 }
 
