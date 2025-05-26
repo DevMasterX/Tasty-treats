7a2992ee2400016c36dfc5f3a5c67fc0d520033f
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-05-26 13:08:29 +0300

    324123

--- src/js/components/rating-modal/rating-stars.js
+++ src/js/components/rating-modal/rating-stars.js
@@ -1,5 +1,19 @@
 function initRatingStars(btn, modalContent) {
-  const stars = modalContent.querySelector('.js-modal-rating-list');
+  const stars = modalContent.querySelector('.js-modal-rating-list').children;
+  console.log('🚀 stars:', stars);
+
+  [...stars].forEach(star => {
+    star.addEventListener('mouseenter', () => {
+      console.log('enter');
+    });
+    star.addEventListener('mouseleave', () => {
+      console.log('leave');
+    });
+
+    star.addEventListener('click', () => {
+      console.log('click');
+    });
+  });
   //   stars.forEach(star);
 }
 
