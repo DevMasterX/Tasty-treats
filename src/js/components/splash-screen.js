1f526a2629616127bd47da467dc17de5d2f85e67
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-06-30 12:36:34 +0300

    splash screen

--- src/js/components/splash-screen.js
+++ src/js/components/splash-screen.js
@@ -1,10 +1,22 @@
 function initSplashScreen() {
-  window.addEventListener('load', () => {
-    const splash = document.querySelector('.splash-screen');
+  const splash = document.querySelector('.splash-screen');
+  const isFirstVisit = !sessionStorage.getItem('visited');
+  splash.style.display = 'none';
+
+  if (isFirstVisit) {
+    splash.style.display = 'flex';
     setTimeout(() => {
       splash.classList.add('is-hidden');
+      splash.setAttribute('aria-hidden', 'true');
+
+      splash.addEventListener('transitionend', e => {
+        if (e.propertyName === 'opacity') {
+          splash.style.display = 'none';
+        }
+      });
     }, 2000);
-  });
+    sessionStorage.setItem('visited', 'true');
+  }
 }
 
 export { initSplashScreen };
